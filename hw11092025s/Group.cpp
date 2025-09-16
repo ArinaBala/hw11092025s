44ce24cdaf00f83b3b41f3087f545222a9514ef8
ArinaBala <arinabalashova.od@gmail.com>
2025-09-16 22:07:53 +0300

    Group.cpp

--- hw11092025s/Group.cpp
+++ hw11092025s/Group.cpp
@@ -4,6 +4,8 @@
 Group::Group()
 {
     name = nullptr;
+    list = nullptr;
+    subList = nullptr;
     countOfStudents = 0;
     countOfSubj = 0;
 }
@@ -15,6 +17,83 @@ Group::Group(const char* n, int studCount, int subjCount)
 
     countOfStudents = studCount;
     countOfSubj = subjCount;
+
+    list = new Student[countOfStudents];
+    subList = new Subject[countOfSubj];
+}
+
+Group::Group(const Group& obj)
+{
+    if (obj.name != nullptr)
+    {
+        name = new char[strlen(obj.name) + 1];
+        strcpy_s(name, strlen(obj.name) + 1, obj.name);
+    }
+    else
+    {
+        name = nullptr;
+    }
+
+    countOfStudents = obj.countOfStudents;
+    countOfSubj = obj.countOfSubj;
+
+    if (countOfStudents > 0)
+    {
+        list = new Student[countOfStudents];
+        for (int i = 0; i < countOfStudents; i++)
+        {
+            list[i] = obj.list[i]; 
+        }
+    }
+    else
+    {
+        list = nullptr;
+    }
+
+    if (countOfSubj > 0)
+    {
+        subList = new Subject[countOfSubj];
+        for (int i = 0; i < countOfSubj; i++)
+        {
+            subList[i] = obj.subList[i]; 
+        }
+    }
+    else
+    {
+        subList = nullptr;
+    }
+}
+
+Group::~Group()
+{
+    delete[] name;
+    delete[] list;
+    delete[] subList;
+}
+
+void Group::Init(const char* n, int studCount, int subjCount)
+{
+    if (name != nullptr)
+    {
+        delete[] name;
+    }
+    if (list != nullptr)
+    {
+        delete[] list;
+    }
+    if (subList != nullptr)
+    {
+        delete[] subList;
+    }
+
+    name = new char[strlen(n) + 1];
+    strcpy_s(name, strlen(n) + 1, n);
+
+    countOfStudents = studCount;
+    countOfSubj = subjCount;
+
+    list = new Student[countOfStudents];
+    subList = new Subject[countOfSubj];
 }
 
 void Group::Print()
@@ -26,39 +105,19 @@ void Group::Print()
     
     else
     {
-    
         cout << "Group: none" << endl;
-    
     }
 
-    cout << "Subjects: " << countOfSubj  << endl;
+    cout << "Subjects: " << countOfSubj << endl;
     for (int i = 0; i < countOfSubj; i++)
     {
         subList[i].Print();
     }
 
-    cout << "Students:" << countOfStudents << endl;
+    cout << "Students: " << countOfStudents << endl;
+
     for (int i = 0; i < countOfStudents; i++)
     {
         list[i].Print();
     }
-}
-
-void Group::Init(const char* n, int studCount, int subjCount)
-{
-    if (name != nullptr)
-    {
-        delete[] name;
-    }
-
-    name = new char[strlen(n) + 1];
-    strcpy_s(name, strlen(n) + 1, n);
-
-    countOfStudents = studCount;
-    countOfSubj = subjCount;
-}
-
-Group::~Group()
-{
-    delete[] name;
 }
