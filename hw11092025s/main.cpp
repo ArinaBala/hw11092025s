a2759c7b557ef5b503a348ac6bcc790b022a942e
ArinaBala <arinabalashova.od@gmail.com>
2025-09-16 22:12:14 +0300

    main.cpp

--- hw11092025s/main.cpp
+++ hw11092025s/main.cpp
@@ -1,29 +1,40 @@
-#include "Student.h"
 #include <iostream>
+#include "Student.h"
+#include "Subject.h"
+#include "Group.h"
 using namespace std;
 
 int main()
 {
-    
-    Student st1;
-    st1.Print(); 
+    cout << "Subject" << endl;
+    Subject sub1("Math");
+    sub1.Print();
 
+    Subject sub2 = sub1; 
+    sub2.Print();
     cout << endl;
 
-   
-    Student st2("Nastya", 3); 
-    st2.Print(); 
+    cout << "Student" << endl;
+    Student st1("Nastya", 3);
+    st1.SetMark(0, 12);
+    st1.SetMark(1, 10);
+    st1.SetMark(2, 8);
+    st1.Print();
 
-    cout << endl;
-
-   
-    st2.SetMark(0, 12);
-    st2.SetMark(1, 10);
-    st2.SetMark(2, 8);
-
+    Student st2 = st1; 
     st2.Print();
+    cout << "Average st1: " << st1.GetAverage() << endl;
+    cout << "Average st2: " << st2.GetAverage() << endl;
+    cout << endl;
 
-    cout << "Average mark: " << st2.GetAverage() << endl;
+    cout << "Group" << endl;
+    Group g1("Group-241", 2, 2);
+    g1.Init("Group-241", 2, 2); 
+
+    g1.Print();
+
+    Group g2 = g1; 
+    g2.Print();
 
     return 0;
 }
